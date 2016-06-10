f1115d79ecd50210edc60d6c026a971551d4802d
calicode <andrew.schmidthuber@gmail.com>
2016-06-10 00:03:26 +0000

    Basic request handling, working on api request. Trying to use nodes builtin http.get

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
+// TODO - CHANGE API KEY IN .ENV TO NON PERSONAL. API REQ FUNCTION. REQ/DAY COUNTER. 
+// process.env.API_KEY
 'use strict'
 var express = require ('express'); 
 var app = express();
+var http = require('http');
 var serverPort = process.env.PORT ||  8080;
-var latitude,longitude;
-app.listen(serverPort, ()=> {console.log("Server listening on ", serverPort); } );
+require ('dotenv').load();
+var latitude,longitude,apiUrl;
 
+
+
+app.listen(serverPort, ()=> {console.log("Server listening on ", serverPort); } );
 app.get('/weather/latitude/:latitude/longitude/:longitude', function (req,res){
 latitude = req.params.latitude;
 longitude = req.params.longitude;
-if (!isNaN(+latitude) && !isNaN(+longitude) ) { res.end("you sent 2 numbers, great"); } else { res.send("One of those can't be converted to a number"+ latitude+" "+ longitude); }
+		if (!isNaN(+latitude) && !isNaN(+longitude) ) {
+		apiUrl = 'http://api.openweathermap.org/data/2.5/weather?lat='+latitude+'&lon='+longitude+'&appid='+process.env.API_KEY;
+		console.log(apiUrl);
+		
+		http.get(apiUrl,(res)=>{
+		    res.resume();
+		    }).on('error', (e) => {
+	              console.log(`Got error: ${e.message}`);
+		}).on('data',(data) => { console.log(data);});
+			
+          } 
+		else { 
+			res.json({result:"One of your values was not a number, you sent "+ latitude+" "+ longitude}); 
+		}
 
 });
