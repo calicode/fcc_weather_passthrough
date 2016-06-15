9a9d3ae9d58c130f62aa70a6d2f0ccaa75ef1749
calicode <andrew.schmidthuber@gmail.com>
2016-06-15 23:52:50 +0000

    added error handling, default behavior for / requests, request count

--- index.js
+++ index.js
@@ -1,35 +1,51 @@
 // TODO - CHANGE API KEY IN .ENV TO NON PERSONAL. API REQ FUNCTION. REQ/DAY COUNTER. 
 // process.env.API_KEY
 'use strict'
-var express = require ('express'); 
+var express = require('express');
 var app = express();
-var serverPort = process.env.PORT ||  8080;
+var serverPort = process.env.PORT || 8080;
 var http = require('http');
-require ('dotenv').load();
-var latitude,longitude,apiUrl;
+require('dotenv').load();
+var latitude, longitude, apiUrl, reqCount;
+reqCount = 0;
+var baseUrl = "whatever.com";
+
+function apiCall(lat, long, res) {
+	apiUrl = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&appid=' + process.env.API_KEY;
+	console.log(apiUrl);
+
+	http.get(apiUrl, (response) => {
+
+		response.on('data', (chunk) => {
+			res.send(chunk.toString('ascii'));
+		}).on('error', (e) => {
+			console.log("error was", e.message);
+		});
+	});
 
 
+};
 
-app.listen(serverPort, ()=> {console.log("Server listening on ", serverPort); } );
-
-app.get('/weather/latitude/:latitude/longitude/:longitude', function (req,res){
-
+app.listen(serverPort, () => {
+	console.log("Server listening on ", serverPort);
+});
+app.get('/', function(req, res) {
+	res.send("To use this application make a request such as " + baseUrl + " /weather/latitude/45/longitude/45");
+});
+app.get('/weather/latitude/:latitude/longitude/:longitude', function(req, res) {
+	reqCount++;
+	console.log('There have been ', reqCount, ' requests since the last restart');
 	latitude = req.params.latitude;
 	longitude = req.params.longitude;
 
-		if (!isNaN(+latitude) && !isNaN(+longitude) ) {
-		apiUrl = 'http://api.openweathermap.org/data/2.5/weather?lat='+latitude+'&lon='+longitude+'&appid='+process.env.API_KEY;
-		console.log(apiUrl);
-		
-	                //if (error || response.statusCode  !== 200) {console.log ("Problem with request", error, response.statusCode); res.end();} 
-			//else { 
-	       http.get(apiUrl, (response)=> {
-	       response.on('data', (chunk)=> {res.send(chunk.toString('ascii')); });
-	       });         
-			
-          } 
-		else { 
-			res.json({result:"One of your values was not a number, you sent "+ latitude+" "+ longitude}); 
-		}
+	if (!isNaN(+latitude) && !isNaN(+longitude)) {
+		apiCall(latitude, longitude, res);
+
+	} else {
+
+		res.json({
+			result: "One of your values was not a number, you sent " + latitude + " " + longitude
+		});
+	}
 
 });
